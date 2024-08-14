4ece8551c45327e7b317899488a83f307fbff4bf
Parambrata Chatterjee <161926059+Parambrata31@users.noreply.github.com>
2024-08-14 22:25:50 +0530

    Add files via upload

---
+++ Week_3_Exercises/2. Employee Management System - Creating Entities/EmployeeManagementSystem/src/main/java/com/code/employee/entity/Employee.java
@@ -0,0 +1,35 @@
+package com.code.employee.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+@Entity
+@Table(name="employees")
+@Getter
+@Setter
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(name="name",nullable = false)
+    private String name;
+
+    @Column(name="email",nullable = false, unique = true)
+    private String email;
+    
+    @Column(name="salary",nullable = false)
+    private double salary;
+
+    @ManyToOne
+    @JoinColumn(name = "department_id", nullable = false)
+    private Department department;
+}
